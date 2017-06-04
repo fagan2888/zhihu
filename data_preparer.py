3d155ace4e19c02e03fa282518c9201ecd4122b1
foursking <foursking@gmail.com>
2017-06-04 13:07:59 +0800

    add

--- data_preparer.py
+++ data_preparer.py
@@ -9,8 +9,17 @@ import config
 from utils import pkl_utils
 
 def main():
-	dfTrain = pd.read_csv(config.TRAIN_DATA)
-	print dfTrain.head(10)
+	dfLabel = pd.read_csv(config.TRAIN_DATA_LABEL, sep='\t', names=['qid', 'tids'])
+	dfTrain = pd.read_csv(config.TRAIN_DATA, sep='\t', names=['qid', 'chars', 'words'])
+	dfTest = pd.read_csv(config.TEST_DATA, sep='\t', names=['qid', 'chars', 'words'])
+	dfTopic = pd.read_csv(config.TOPIC_DATA, sep='\t', names=['tid', 'ptids', 'title_chars', 'title_words', 'desc_chars', 'desc_words'])
+	dfCharsEmbeddings = pd.read_csv(config.CHAR_EMBEDDING, sep='\t', names=['cid', 'embeddings'])
+	dfWordsEmbeddings = pd.read_csv(config.WORD_EMBEDDING, sep='\t', names=['wid', 'embeddings'])
+
+	#
+	print("Lenght of Train set %d" % len(dfTrain))
+	print("Length of Topic %d" % len(dfTopic))
+	print("Length of Test set %d" % len(dfTest))
 
 if __name__ == "__main__":
 	main()
