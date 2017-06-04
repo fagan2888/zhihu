e25f1138324776a6a6c6cd762215a35a3c30a3b4
foursking <foursking@gmail.com>
2017-06-04 11:31:31 +0800

    add

--- config.py
+++ config.py
@@ -3,15 +3,15 @@
 # ------------------------ PATH ------------------------
 ROOT_DIR = "../"
 
-DATA_DIR = "%s/Data"%ROOT_DIR
-CLEAN_DATA_DIR = "%s/Clean"%DATA_DIR
+DATA_DIR = "%s/Data"%ROOT_DIR"
+CLEAN_DATA_DIR = "%s/Clean"%DATA_DIR"
 
 # ------------------------ DATA ------------------------
 # provided data
-TOPIC_DATA＝ "%s/topic_info.txt"%DATA_DIR
-CHAR_EMBEDDING = "%s/char_embedding.txt"%DATA_DIR
-WORD_EMBEDDING = "%s/word_embedding.txt"%DATA_DIR
-TRAIN_DATA = "%s/question_topic_train_set.txt"%DATA_DIR
-TRAIN_DATA_INFO = "%s/question_train_set.txt"%DATA_DIR
-TEST_DATA = "%s/question_eval_set.txt"%DATA_DIR
+TOPIC_DATA＝ "%s/topic_info.txt"%DATA_DIR"
+CHAR_EMBEDDING = "%s/char_embedding.txt"%DATA_DIR"
+WORD_EMBEDDING = "%s/word_embedding.txt"%DATA_DIR"
+TRAIN_DATA = "%s/question_topic_train_set.txt"%DATA_DIR"
+TRAIN_DATA_INFO = "%s/question_train_set.txt"%DATA_DIR"
+TEST_DATA = "%s/question_eval_set.txt"%DATA_DIR"
 
